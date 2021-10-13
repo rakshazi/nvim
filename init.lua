930fd64656b7df23277e320731878fae7ab3f8a7
rakshazi <osi@rakshazi.me>
2021-10-13 22:20:32 +0300

    themes, plugins and codestyle

--- init.lua
+++ init.lua
@@ -12,16 +12,18 @@ autocmd BufWritePost init.lua PackerCompile
 augroup end
 ]], false)
 
+-- Plugins
 local use = require('packer').use
 require('packer').startup(function()
 	use 'wbthomason/packer.nvim' -- Package manager
 	use 'sheerun/vim-polyglot' -- identation
 	use 'b3nj5m1n/kommentary' -- comments
 	use 'fatih/vim-go' -- Golang
+	use 'pearofducks/ansible-vim' -- Ansible
 	-- UI to select things (files, grep results, open buffers...)
 	use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
 	use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-	use 'joshdick/onedark.vim' -- Theme inspired by Atom
+	use 'rakshazi/darcula' -- colorscheme
 	use 'itchyny/lightline.vim' -- Fancier statusline
 	use { 'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons' } -- nerdtree-like
 	use { 'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons' } -- buffer line
@@ -42,97 +44,101 @@ require('packer').startup(function()
 	use 'L3MON4D3/LuaSnip' -- Snippets plugin
 end)
 
---Incremental live completion (note: this is now a default on master)
+-- Incremental live completion (note: this is now a default on master)
 vim.o.inccommand = 'nosplit'
 
---Set highlight on search
+-- Set highlight on search
 vim.o.hlsearch = false
 
---Make line numbers default
+-- Make line numbers default
 vim.wo.number = true
 
---Do not save when switching buffers (note: this is now a default on master)
+-- Do not save when switching buffers (note: this is now a default on master)
 vim.o.hidden = true
 
---Enable mouse mode
+-- Enable mouse mode
 -- vim.o.mouse = 'a'
 
---Enable break indent
+-- Enable break indent
 vim.o.breakindent = true
 
---Save undo history
+-- Save undo history
 vim.opt.undofile = true
 
---Case insensitive searching UNLESS /C or capital in search
+-- Case insensitive searching UNLESS /C or capital in search
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
---Decrease update time
+-- Decrease update time
 vim.o.updatetime = 250
 vim.wo.signcolumn = 'yes'
 
---Set colorscheme (order is important here)
-vim.o.termguicolors = true
-vim.g.onedark_terminal_italics = 2
-vim.cmd [[colorscheme onedark]]
+-- Folding
+vim.o.foldmethod = 'syntax'
+vim.o.foldnestmax = 1
 
---Set bufferbar
+-- Set colorscheme (order is important here)
+vim.o.termguicolors = true
+vim.cmd [[colorscheme darcula]]
+
+-- Set bufferbar
 require("bufferline").setup{
 	options = {
 		diagnostics = "nvim_lsp",
 		show_buffer_icons = true,
 		show_buffer_close_icons = false,
 		show_close_icon = false,
-		always_show_bufferline = true 
+		always_show_bufferline = true
 	}
 }
 
---Set bufferbar hotkeys
+-- Set bufferbar hotkeys
 vim.api.nvim_set_keymap('n', '<C-w>', ':w|bd<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-n>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-p>', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
 
---Set statusbar
+-- Set statusbar
 vim.g.lightline = {
-	colorscheme = 'onedark',
 	active = { left = { { 'mode', 'paste' }, { 'gitbranch', 'readonly', 'filename', 'modified' } } },
 	component_function = { gitbranch = 'fugitive#head' },
 }
 
---Remap space as leader key
+-- Remap space as leader key
 vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
---Remap for dealing with word wrap
+-- Remap for dealing with word wrap
 vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
 vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
 
---Ctrl+S
+-- Ctrl+S
 vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
 
 -- Highlight on yank
-vim.api.nvim_exec(
-	[[
+vim.api.nvim_exec([[
 augroup YankHighlight
 autocmd!
 autocmd TextYankPost * silent! lua vim.highlight.on_yank()
 augroup end
-]],
-	false
-	)
+]], false)
+
+-- Remove trailing whitespaces
+vim.api.nvim_exec([[
+autocmd BufWritePre * :%s/\s\+$//e
+]], false)
 
 -- Y yank until the end of line  (note: this is now a default on master)
 vim.api.nvim_set_keymap('n', 'Y', 'y$', { noremap = true })
 
---Map blankline
+-- Map blankline
 vim.g.indent_blankline_char = '┊'
 vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
 vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
 vim.g.indent_blankline_char_highlight = 'LineNr'
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 
---nerdtree-like
+-- nerdtree-like
 vim.api.nvim_set_keymap('', '<C-e>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 vim.g.nvim_tree_quit_on_open = 1
 vim.g.nvim_tree_git_hl = 1
@@ -145,26 +151,32 @@ require('nvim-tree').setup {
 	open_on_setup = true
 }
 
---nerdcommenter
+-- nerdcommenter
 -- alternative keymap: gcc
 vim.api.nvim_set_keymap('i', '<C-_>', '<Plug>kommentary_line_default', {})
 vim.api.nvim_set_keymap('n', '<C-_>', '<Plug>kommentary_line_default', {})
 vim.api.nvim_set_keymap('v', '<C-_>', '<Plug>kommentary_visual_default', {})
 
---vim-go
-vim.g.go_doc_popup_window = 1
-vim.g.go_gopls_enabled =1
-vim.g.go_metalinter_autosave = 1
-vim.g.go_auto_type_info = 1
+-- ansible-vim
+vim.g.ansible_unindent_after_newline = 1
+vim.g.ansible_extra_keywords_highlight = 1
+
+-- vim-go
 vim.g.go_auto_sameids = 1
-vim.g.go_fmt_command = "goimports"
-vim.g.go_metalinter_autosave = 1
-vim.g.go_highlight_types = 1
+vim.g.go_auto_type_info = 1
+vim.g.go_doc_popup_window = 1
+vim.g.go_fmt_command = 'goimports'
+vim.g.go_fmt_experimental = 1
+vim.g.go_gopls_enabled =1
+vim.g.go_highlight_extra_types = 1
 vim.g.go_highlight_fields = 1
+vim.g.go_highlight_function_calls = 1
 vim.g.go_highlight_functions = 1
 vim.g.go_highlight_operators = 1
-vim.g.go_highlight_extra_types = 1
-vim.g.go_highlight_function_calls = 1
+vim.g.go_highlight_types = 1
+vim.g.go_metalinter_autosave = 1
+vim.g.go_metalinter_autosave = 1
+vim.g.go_term_enabled = 1
 
 -- Gitsigns
 require('gitsigns').setup {
@@ -197,7 +209,7 @@ require('telescope').setup {
 		}
 	},
 }
---Add leader shortcuts
+-- Add leader shortcuts
 vim.api.nvim_set_keymap('n', '`', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>sf', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]], { noremap = true, silent = true })
